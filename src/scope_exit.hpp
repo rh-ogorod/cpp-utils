fbfd96d07a45c1c19865e569f6b4664dab49792f
ramblehead <v.rybynok@gmail.com>
2022-08-23 22:58:40 +0100

    Adding src/scope_exit.hpp

---
+++ src/scope_exit.hpp
@@ -0,0 +1,29 @@
+// Hey Emacs, this is -*- coding: utf-8; mode: c++ -*-
+#ifndef __rh_ogorod_cpp_utils_scope_exit_hpp__
+#define __rh_ogorod_cpp_utils_scope_exit_hpp__
+
+#include <vector>
+
+namespace rh_ogorod::cpp_utils {
+
+template <typename F>
+requires std::invocable<F>
+struct ScopeExit {
+  using Finally = F;
+
+  ScopeExit(const Finally&) = delete;
+  auto operator=(const ScopeExit&) -> ScopeExit& = delete;
+
+  explicit ScopeExit(Finally&& finally) : m_finally{std::move(finally)} {}
+
+  ~ScopeExit() {
+    m_finally();
+  }
+
+ private:
+  Finally m_finally;
+};
+
+}  // namespace rh_ogorod::cpp_utils
+
+#endif  // __rh_ogorod_cpp_utils_scope_exit_hpp__
